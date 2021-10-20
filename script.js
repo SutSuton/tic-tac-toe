91a53ea7093253000d231e17e19437c09e0470d8
sutsuton <sutsuton@gmail.com>
2021-10-20 12:29:48 +1100

    further optimisations: 	made sure the game reset the token to X after each game 	moved the resign function out of the clearGrid function

--- script.js
+++ script.js
@@ -10,19 +10,20 @@ const gameBoard = (() => {
     }
   }
 
-  const resignation = () => {
-    playArea = [];
-    gameController.token === "X" ? alert("O wins by resignation") : alert("X wins by resignation");
+  const clearGrid = () => {
+    playArea.splice(0, 9);
     for (let i = 0; i < 9; i++) {
       cells[i].textContent = "";
     }
+    gameController.token = "X";
   }
-  return {playArea, storeGrid, resignation};
+  return {playArea, storeGrid, clearGrid};
 })();
 
 const resignButton = document.querySelector(".resign");
 resignButton.addEventListener("click", function(e) {
-  gameBoard.resignation();
+  gameController.token === "X" ? alert("O wins by resignation") : alert("X wins by resignation");
+  gameBoard.clearGrid();
 })
 
 
@@ -44,7 +45,7 @@ const gameController = (() => {
 
     gameBoard.storeGrid(cells);
     console.log(gameBoard.playArea);
-    checkWin(gameBoard.playArea);
+    setTimeout(checkWin, 500, gameBoard.playArea);
   };
 
   // function to check if anyone has won
@@ -55,14 +56,16 @@ const gameController = (() => {
         (array[0] && array[0] == array[3] && array[0] == array[6])
         ) {
           // array[0] WINS!
-          console.log(array[0] + " wins")
+          alert(array[0] + " wins!");
+          gameBoard.clearGrid();
         } 
     else if (
         (array[8] && array[8] == array[5] && array[8] == array[2]) ||
         (array[8] && array[8] == array[7] && array[8] == array[6])
         ) {
           // array[8] WINS!
-          console.log(array[8] + " wins")
+          alert(array[8] + " wins!");
+          gameBoard.clearGrid();
         }
     else if (
         (array[4] && array[4] == array[1] && array[4] == array[7]) ||
@@ -70,10 +73,12 @@ const gameController = (() => {
         (array[4] && array[4] == array[6] && array[4] == array[2])
         ) {
           // array[4] WINS!
-          console.log(array[4] + " wins")
+          alert(array[4] + " wins!");
+          gameBoard.clearGrid();
         }
     else if (array.filter(n => n).length == 9) {
-      console.log("draw");
+      alert("The game is drawn");
+      gameBoard.clearGrid();
     }
 };
 
